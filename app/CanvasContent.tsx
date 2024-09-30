0eb157172b913dc876320de75f2d2cee0c4a4198
RH12503 <ryanh12503@gmail.com>
2024-09-29 23:10:09 -0700

    Initial - messy

---
+++ app/CanvasContent.tsx
@@ -0,0 +1,217 @@
+"use client";
+
+import React, { RefObject, useEffect, useMemo, useRef, useState } from "react";
+import { Color, Mesh, UniformsLib, UniformsUtils } from "three";
+
+import { Canvas, Vector3, useFrame } from "@react-three/fiber";
+import Image from "next/image";
+import { Bloom, EffectComposer } from "@react-three/postprocessing";
+import { Stats, OrbitControls, Text3D, Stars } from "@react-three/drei";
+import { timerLocal } from "three/webgpu";
+
+const pink = new Color(172 / 70, 17 / 70, 255 / 70);
+const blue = new Color(0 / 70, 206 / 70, 225 / 70);
+
+const vertexShader = `
+  #include <fog_pars_vertex>
+    varying float vY;
+    uniform float time;
+
+    void main() {
+        vY = position.y;
+        #include <begin_vertex>
+        #include <project_vertex>
+        #include <fog_vertex>
+    }
+      `;
+
+const fragmentShader = `
+  #include <fog_pars_fragment>
+    varying float vY;
+    uniform float time;
+
+    void main() {
+        float n  = 0.5+0.5*(sin(vY * 2.0 + time)); 
+
+        vec3 color = mix(vec3(0. / 70., 206. / 70., 225. / 70.),vec3(172. / 70., 17. / 70., 255. / 70.), n);
+
+        gl_FragColor = vec4(color, 1.0);
+
+        #include <fog_fragment>
+    }`;
+
+function GradientCylinder({
+  position,
+  key,
+  time,
+}: {
+  position?: Vector3;
+  key: number;
+  time: number;
+}) {
+  const mesh = useRef<Mesh>(null);
+
+  const uniforms = useMemo(
+    () =>
+      UniformsUtils.merge([
+        {
+          time: { value: 0 },
+        },
+        UniformsLib["fog"],
+      ]),
+    []
+  );
+
+  useEffect(() => {
+    // @ts-ignore
+    mesh.current.material.uniforms.time.value = time;
+  }, [time]);
+
+  return (
+    <mesh
+      position={position}
+      rotation={[0, 0, Math.PI / 2]}
+      key={key}
+      ref={mesh}
+    >
+      <cylinderGeometry args={[0.002, 0.002, 29, 6]} />
+      <shaderMaterial
+        uniforms={uniforms}
+        fragmentShader={fragmentShader}
+        vertexShader={vertexShader}
+        toneMapped={false}
+        fog={true}
+        needsUpdate
+      />
+    </mesh>
+  );
+}
+
+function mix(v1: Color, v2: Color, t: number): Color {
+  return new Color(
+    v1.r * (1 - t) + v2.r * t,
+    v1.g * (1 - t) + v2.g * t,
+    v1.b * (1 - t) + v2.b * t
+  );
+}
+
+export default function CanvasContent({
+  canvasRef,
+}: {
+  canvasRef?: RefObject<HTMLCanvasElement | null>;
+}) {
+  const [time, setTime] = useState(0);
+  useFrame((state, delta) => setTime(time + 0.02));
+  const [fog, setFog] = useState(1);
+  useFrame((state, delta) => {
+    if (fog > 0.25) setFog(fog - 0.01);
+  });
+
+  const aspect =
+    (canvasRef?.current?.width ?? 2) / (canvasRef?.current?.height ?? 1);
+
+  return (
+    <>
+      {<fogExp2 attach="fog" color="black" density={fog} />}
+      {/*        <fog attach="fog" color="black" near={5} far={25} />
+       */}
+      <group rotation={[0, 0, (-Math.PI / 4) * 0]}>
+        {Array.from({ length: 6 }, (item, index) => (
+          <mesh position={[(index - 3) / 1 + 0.5, 1, -2.7]} key={index}>
+            <pointLight
+              color={mix(
+                pink,
+                blue,
+                0.5 + 0.5 * Math.sin((index - 2.5) * 4 + time)
+              )}
+              intensity={0.6}
+            />
+            {/* <sphereGeometry args={[0.01, 10, 10]} />*/}
+          </mesh>
+        ))}
+        {Array.from({ length: 6 }, (item, index) => (
+          <mesh position={[(index - 3) / 1 + 0.5, -0.5, -2.7]} key={index}>
+            <pointLight
+              color={mix(
+                pink,
+                blue,
+                0.5 + 0.5 * Math.sin((index - 2.5) * 4 + time)
+              )}
+              intensity={0.6}
+            />
+            {/* <sphereGeometry args={[0.01, 10, 10]} />*/}
+          </mesh>
+        ))}
+        {Array.from({ length: 10 }, (item, index) => (
+          <GradientCylinder
+            position={[0, 1, 0.1 - index]}
+            key={index}
+            time={time}
+          />
+        ))}
+        {Array.from({ length: 10 }, (item, index) => (
+          <GradientCylinder
+            position={[0, -1, 0.1 - index]}
+            key={index}
+            time={time}
+          />
+        ))}
+        {Array.from({ length: 20 }, (item, index) => (
+          <mesh
+            position={[(index - 10) / 1 + 0.5, 1, -25]}
+            rotation={[Math.PI / 2, 0, 0]}
+            key={index}
+          >
+            <cylinderGeometry args={[0.008, 0.008, 50, 6]} />
+            <meshBasicMaterial
+              color={mix(
+                pink,
+                blue,
+                0.5 + 0.5 * Math.sin((index - 9.5) * 4 + time)
+              )}
+              toneMapped={false}
+            />
+          </mesh>
+        ))}
+        {Array.from({ length: 20 }, (item, index) => (
+          <mesh
+            position={[(index - 10) / 1 + 0.5, -1, -25]}
+            rotation={[Math.PI / 2, 0, 0]}
+            key={index}
+          >
+            <cylinderGeometry args={[0.008, 0.008, 50, 6]} />
+            <meshBasicMaterial
+              color={mix(
+                pink,
+                blue,
+                0.5 + 0.5 * Math.sin((index - 9.5) * 4 + time)
+              )}
+              toneMapped={false}
+            />
+          </mesh>
+        ))}
+      </group>
+
+      <Text3D
+        font="/Orbitron_Regular.json"
+        position={[aspect > 1.4 ? -2.25 : -0.6, aspect > 1.4 ? 0.2 : 0.5, -4]}
+        size={aspect > 1.4 ? 0.35 : 0.18}
+        bevelEnabled
+        curveSegments={12}
+        bevelThickness={0.01}
+        bevelSize={0.01}
+        bevelOffset={0}
+        bevelSegments={10}
+        height={0.05}
+      >
+        Immerse {aspect < 1.4 && "\n "}the Bay
+        <meshPhongMaterial color={"white"} shininess={100} specular={"white"} />
+      </Text3D>
+      <EffectComposer>
+        <Bloom intensity={2.0} luminanceThreshold={0} kernelSize={3} />
+      </EffectComposer>
+      <OrbitControls enableRotate enablePan />
+      {/*<Stats />*/}
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,50 @@
+"use client";
+
+import React, { useRef } from "react";
+import { Color, Mesh, UniformsLib, UniformsUtils } from "three";
+
+import { Canvas, Vector3, useFrame } from "@react-three/fiber";
 import Image from "next/image";
+import { Bloom, EffectComposer } from "@react-three/postprocessing";
+import { Stats, OrbitControls } from "@react-three/drei";
+import CanvasContent from "./CanvasContent";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const ref = useRef<HTMLCanvasElement>(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+  return (
+    <div className="h-screen w-screen bg-black">
+      <Canvas
+        shadows
+        style={{ background: "transparent" }}
+        camera={{
+          position: [0, 0, 0.1],
+          fov: 50,
+        }}
+        ref={ref}
+      >
+        <CanvasContent canvasRef={ref} />
+      </Canvas>
+      <div className="absolute z-10 top-0 left-0 w-screen h-screen">
+        <div className="h-1/2"></div>
+        <div className="font-bold text-base md:text-2xl text-white flex items-center justify-center">
+          NOVEMBER 8-10 • STANFORD, CA
+        </div>
+        <div className="my-2 flex flex-col md:flex-row items-center justify-center">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href={"https://93sypddfktx.typeform.com/to/yZWbcdg4"}
+            className="font-semibold text-white text-2xl underline p-1 m-2"
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
+            Apply
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href={""}
+            className="font-semibold text-white text-2xl underline p-1 m-2"
           >
-            Read our docs
+            Sponsor
           </a>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
