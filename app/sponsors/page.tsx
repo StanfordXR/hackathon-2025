56c813fcfd8322b8c5be0c217553d087155147c2
Jolie837 <75588353+Jolie837@users.noreply.github.com>
2025-09-19 11:01:03 -0700

    Update page.tsx

--- app/sponsors/page.tsx
+++ app/sponsors/page.tsx
@@ -11,9 +11,10 @@ export default function Home() {
   return (
     <main id="main">
       <div className="w-screen min-h-screen bg-immersive md:p-[200px] md:pt-[120px] pt-[120px] pb-[60px] flex justify-center items-center flex-col">
-        {sponsors.map((tier, index) => (
+        {/*{sponsors.map((tier, index) => (
           <Tier tier={tier} key={index} />
-        ))}
+        ))}*/}
+        <div className="text-white text-2xl font-bold">TBD</div>
       </div>
     </main>
   );
