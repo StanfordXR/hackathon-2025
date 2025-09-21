89ad87058d59cc45e37a73cb65e4d0c681c15167
Victor Chen <89617516+VictorChenCA@users.noreply.github.com>
2025-09-21 16:14:19 -0700

    Refactor Sponsors

--- app/tracks/page.tsx
+++ app/tracks/page.tsx
@@ -12,16 +12,18 @@ export default function Home() {
         <div className={`text-white font-semibold font-orbitron text-3xl mb-6`}>
           Tracks
         </div>
+        <div className="text-white font-orbitron text-3xl mt-24 p-32">TBD</div>
+
         <div className={`w-[80%] mx-10 h-full`}>
+          {/* This a temporarily removed Tracks display
+
           <div className={`text-white font-orbitron text-3xl`}>
             Primary Tracks
           </div>
           <hr className={`shadow-glowing my-1 mb-4`}></hr>
           <div className={`text-white font-ptsans text-xl`}>
             <div className="flex flex-row justify-between border-b border-white border-opacity-25 mt-4">
-              <span className={`font-semibold`}>
-                TBD
-              </span>
+              <span className={`font-semibold`}>TBD</span>
               <span>TBD</span>
             </div>
             TBD
@@ -33,9 +35,7 @@ export default function Home() {
           <hr className={`shadow-glowing my-1 mb-4`}></hr>
           <div className={`text-white font-ptsans text-xl`}>
             <div className="flex flex-row justify-between border-b border-white border-opacity-25 mt-4">
-              <span className={`font-semibold`}>
-                TBD
-              </span>
+              <span className={`font-semibold`}>TBD</span>
               <span>TBD</span>
             </div>
             TBD
@@ -44,11 +44,12 @@ export default function Home() {
             </div>
             TBD
             <br />
-          </div>
+          </div>*/}
+
           <div
             className={`mt-[100px] text-white underline font-ptsans text-2xl flex align-center justify-center`}
           >
-            <a href="/slides.pdf">See slides for more info</a>
+            <a href="/slides.pdf">See Immerse The Bay 2024's Sponsor Deck</a>
           </div>
         </div>
       </div>
