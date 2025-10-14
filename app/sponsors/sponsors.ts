c37370a9036d59c4c80bcd48d749dbbf7c63bae8
Victor Chen <89617516+VictorChenCA@users.noreply.github.com>
2025-10-14 04:24:47 -0700

    Update Sponsors

--- app/sponsors/sponsors.ts
+++ app/sponsors/sponsors.ts
@@ -14,6 +14,7 @@ export const sponsors: SponsorTier[] = [
 		sponsors: [
 			{ name: "ByteDance", imgsrc: "/sponsors/ByteDance.png" },
 			{ name: "OpenBCI", imgsrc: "/sponsors/OpenBCI.png" },
+			{ name: "Meta", imgsrc: "/sponsors/Meta.png" },
 		],
 	},
 	{
